3b5f481cac5b49122c9035ab9a4fa50ac8bca508
Habib Afolabi <habibwritescode@gmail.com>
2021-12-14 16:59:18 +0100

    fix for fonts and spacing

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function openTab(tabName) {
     const tabButtonTwo = document.getElementById("tab-button-two")
 
     if(tabName === 'tabOne'){
-        tabBox.style.backgroundImage = "url('./assets/bot.jpg')";
+        tabBox.style.backgroundImage = "url('./assets/bot1.png')";
         tabButtonOne.classList.add("tab__list-item--primary")
         tabButtonOne.classList.remove("tab__list-item--secondary")
 
         tabButtonTwo.classList.add("tab__list-item--secondary")
         tabButtonTwo.classList.remove("tab__list-item--primary")
     }else{
-        tabBox.style.backgroundImage = "url('./assets/car2.jpg')";
+        tabBox.style.backgroundImage = "url('./assets/bot2.jpg')";
         tabButtonOne.classList.add("tab__list-item--secondary")
         tabButtonOne.classList.remove("tab__list-item--primary")
         
